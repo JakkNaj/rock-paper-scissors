041ced082e45c9ead1cb48bf169e48d487e582b2
Jakub Najman <jakub@najman.cz>
2023-07-24 11:06:22 +0200

    game working with output in browser console

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-console.log = function(message) {
-    document.getElementById('logArea').innerHTML = message;
-};
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
@@ -10,8 +9,6 @@ function getComputerChoice() {
 function playRound(playerChoice) {
     let computer = getComputerChoice();
     let player = playerChoice;
-    console.log("player choice: " + player);
-    return
 
     if ( computer === player ) {
         console.log("TIE, play again");
@@ -20,31 +17,41 @@ function playRound(playerChoice) {
         if (computer === "rock") {
             switch(player) {
                 case "paper": console.log("You win this round!, paper beats rock");
-                            return "player";
+                            return playerScore++;
                 case "scissors": console.log("You lose this round!, rock beats scissors");
-                            return "computer";
+                            return computerScore++;
             }
         }
         if (computer === "paper") {
             switch(player) {
                 case "scissors": console.log("You win this round!, scissors beats paper");
-                            return "player";
+                    return playerScore++;
                 case "rock": console.log("You lose this round!, paper beats rock");
-                            return "computer";
+                    return computerScore++;
             }
         }
         if (computer === "scissors") {
             switch(player) {
                 case "rock": console.log("You win this round!, rock beats scissors");
-                            return "player";
+                    return playerScore++;
                 case "paper": console.log("You lose this round!, scissors beats paper");
-                            return "computer";
+                    return computerScore++;
             }
         }
     }
 }
-function checkScore(a, b) {
-    return a - b >= 2;
+
+function checkCount(){
+    if (playerScore >= 5) {
+        console.log("Player win!");
+        playerScore = 0;
+        computerScore = 0;
+    } else if (computerScore >= 5) {
+        console.log("Computer win!");
+        playerScore = 0;
+        computerScore = 0;
+    }
+    console.log("P: " + playerScore + "   C: " + computerScore);
 }
 
 function showGame(){
@@ -61,4 +68,18 @@ document.getElementById("mainSection").style.display = "none";
 
 document.querySelector('button[id="play"]').addEventListener("click", showGame);
 
-document.getElementById("paper").addEventListener("click", playRound("paper"));
+document.getElementById("paper").addEventListener("click",  () => {
+    playRound("paper");
+    checkCount();
+})
+
+document.getElementById("scissors").addEventListener("click",  () => {
+    playRound("scissors");
+    checkCount();
+})
+
+document.getElementById("rock").addEventListener("click",  () => {
+    playRound("rock");
+    checkCount();
+})
+
